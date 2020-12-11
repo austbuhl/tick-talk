26ce718bf53b42f59ef388674eb17dcc51cded5d
AB <austinbuhler@gmail.com>
2020-12-10 22:30:37 -0500

    Messages fetching & creating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import Pusher from 'pusher-js'
 import { Chat } from './components/chat/Chat'
 import { Sidebar } from './components/sidebar/Sidebar'
 import { Wrapper } from './App.styles'
+import axios from './axios'
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get('/messages').then((resp) => {
+      setMessages(resp.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('1e8a1765529bbbac589e', {
+      cluster: 'us2'
+    })
+
+    const channel = pusher.subscribe('messages')
+    channel.bind('inserted', (data) => {
+      setMessages([...messages, data])
+    })
+
+    return () => {
+      channel.unbind()
+      channel.unsubscribe('messages')
+    }
+  }, [messages])
+
+  console.log(messages)
   return (
     <Wrapper>
       <div className='app-body'>
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </Wrapper>
   )
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Wrapper, Header, Body, Footer } from './Chat.styles'
 import { Avatar, IconButton } from '@material-ui/core'
 import {
@@ -8,8 +8,23 @@ import {
   InsertEmoticon,
   Mic
 } from '@material-ui/icons'
+import axios from '../../axios'
 
-export const Chat = () => {
+export const Chat = ({ messages }) => {
+  const [msg, setMsgValue] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+    await axios.post('/messages', {
+      message: msg,
+      name: 'Austin',
+      timestamp: new Date().toUTCString(),
+      sent: true
+    })
+
+    setMsgValue('')
+  }
+
   return (
     <Wrapper>
       <Header>
@@ -32,29 +47,27 @@ export const Chat = () => {
       </Header>
 
       <Body>
-        <p className='chat-message'>
-          <span className='chat-name'>Austin</span>
-          This is a message
-          <span className='chat-timestamp'>{new Date().toUTCString()}</span>
-        </p>
-        <p className='chat-message chat-sent'>
-          <span className='chat-name'>Austin</span>
-          This is a message
-          <span className='chat-timestamp'>{new Date().toUTCString()}</span>
-        </p>
-        <p className='chat-message'>
-          <span className='chat-name'>Austin</span>
-          This is a message
-          <span className='chat-timestamp'>{new Date().toUTCString()}</span>
-        </p>
+        {messages.map((message) => (
+          <p
+            key={message._id}
+            className={`chat-message ${message.sent && 'chat-sent'}`}
+          >
+            <span className='chat-name'>{message.name}</span>
+            {message.message}
+            <span className='chat-timestamp'>{message.timestamp}</span>
+          </p>
+        ))}
       </Body>
 
       <Footer>
         <InsertEmoticon />
-        <form>
-          <input placeholder='Type a message' type='text' />
-          {/* do we need this button? */}
-          <button>Send a message</button>
+        <form onSubmit={sendMessage}>
+          <input
+            onChange={(e) => setMsgValue(e.target.value)}
+            value={msg}
+            placeholder='Type a message'
+            type='text'
+          />
         </form>
         <Mic />
       </Footer>
